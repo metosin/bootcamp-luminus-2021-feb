0f398be0222a5d77c0482e7867fe223c0c53a4c5
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2021-02-18 15:24:24 +0200

    Remove docker-compose etc

--- dev-deps-up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker-compose -f docker-compose-dev.yml down
-
-docker-compose -f docker-compose-dev.yml up -d
-
-lein run migrate
--- test-deps-up.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker-compose -f docker-compose-test.yml down
-
-docker-compose -f docker-compose-test.yml up -d
